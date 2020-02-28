cb3d8317fea17007d1b303351fdd98d42d2f9d11
pornsukon <pornsukon@kkumail.com>
2020-02-28 11:57:15 +0700

    Merge branch 'master' of https://github.com/thiatham/softwareENMerge branch 'master' of https://github.com/thiatham/softwareEN

--- functions/public/js/demo/chart-pie-demo.js
+++ functions/public/js/demo/chart-pie-demo.js
@@ -7,11 +7,11 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["โครงการที่จัดแล้ว", "โครงการที่ยังไม่จัด"],
     datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      data: [35,65],
+      backgroundColor: ['#4e73df', '#e74a3b'],
+      hoverBackgroundColor: ['#6610f2','#bd2112'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
@@ -30,6 +30,6 @@ var myPieChart = new Chart(ctx, {
     legend: {
       display: false
     },
-    cutoutPercentage: 80,
+    cutoutPercentage: 50,
   },
 });
